134e9a1d5aef168574ee01bef37efaed8e6dff68
johnnyhsu1106 <johnnyhsu1106@gmail.com>
2019-01-25 23:56:00 -0500

    update solutions

--- 121_word_ladder_II.py
+++ 121_word_ladder_II.py
@@ -4,11 +4,11 @@ find all shortest transformation sequence(s) from start to end, such that:
 
 Only one letter can be changed at a time
 Each intermediate word must exist in the dictionary
- Notice
+Notice
 
 All words have the same length.
 All words contain only lowercase alphabetic characters.
-Have you met this question in a real interview? Yes
+
 Example
 Given:
 start = "hit"
@@ -30,82 +30,89 @@ class Solution:
     """
     def findLadders(self, start, end, dictionary):
 
-        result = []
-        distances = defaultdict(int) # a dict to store word(key) and distance from end
-        mapping = defaultdict(list) # a dict to store word(key) and set of next words (value)
+        results = []
+        words_distance = defaultdict(int) # a dict to store word(key) and distance from end
+        next_words_mapping = defaultdict(list) # a dict to store word(key) and set of next words (value)
         dictionary.add(start) # add the start to dictionary (must-do)
         dictionary.add(end) # add the end to dictionary (must-do)
+        path = []
+        # use bfs from end to start to get number of each word's transformation
+        self.bfs(start, end, dictionary, words_distance, next_words_mapping)
+        # use dfs from start to end to get all the result
+        self.dfs(start, end, path, words_distance, next_words_mapping, results)
 
-        self.bfs(start, end, dictionary, distances, mapping)
-        self.dfs(start, end, [], distances, mapping, result)
-
-        return result
+        return results
 
 
-    def dfs(self, current_word, end, path, distances, mapping, result):
+        def bfs(self, start, end, dictionary, words_distance, next_words_mapping):
+            '''
+            use BFS to traverse all word transformation from end to begin.
+            store the distance (the step of transformation)
+            for each word in Dictionary/Hash Map, called words_distance
+            words_distance = {word: distance, ...}
+            next_words_mapping = {word: [next_word1, next_word2,...]}
+            '''
+
+            queue = deque([end])
+            visited_words = set([end])
+            distance = 0
+
+            while queue:
+                size = len(queue)
+
+                # traverse by level
+                for i in range(size):
+                    word = queue.popleft()
+                    words_distance[word] = distance
+
+                    next_words = self._get_next_words(word, dictionary)
+                    next_words_mapping[word] = next_words
+
+                    for next_word in next_words:
+                        if next_word not in visited_words:
+                            queue.append(next_word)
+                            visited_words.add(next_word)
+                distance += 1
+
+
+        def _get_next_words(self, word, dictionary):
+
+            next_words = []
+            NUM_OF_ALPHABETS = 26
+
+            for i in range(len(word)):
+                for j in range(NUM_OF_ALPHABETS - 1):
+                    index = (ord(word[i]) + j - ord('a')) % NUM_OF_ALPHABETS + ord('a')
+                    new_char = chr(index)
+                    new_word = self._replace_char(i,new_char, word)
+
+                    if new_word in dictionary:
+                        next_words.append(new_word)
+
+            return next_words
+
+        def _replace_char(self, i, char, word):
+            return word[ : i] + char + word[i + 1 : ]
+
+
+    def dfs(self, current_word, end, path, words_distance, next_words_mapping, results):
         '''
         use DFS to traverse each word and next word recursively, following the distance
         '''
         if current_word == end:
             path.append(end)
-            result.append(list(path))
+            results.append(path.copy())
             path.pop()
             return
 
-        for next_word in mapping[current_word]:
-            if distances[current_word] == distances[next_word] + 1:
+        for next_word in next_words_mapping[current_word]:
+            if words_distance[current_word] == words_distance[next_word] + 1:
                 path.append(current_word)
-                self.dfs(next_word, end, path, distances, mapping, result)
+                self.dfs(next_word, end, path, words_distance, next_words_mapping, results)
                 path.pop()
 
 
-    def bfs(self, start, end, dictionary, distances, mapping):
-        '''
-        use BFS to traverse all word transformation from end to begin.
-        store the distance (the step of transformation)
-        for each word in Dictionary/Hash Map, called distances
-        distances = {word: distance, ...}
-        mapping = {word: [next_word1, next_word2,...]}
-        '''
 
-        queue = deque([end])
-        visited = set([end])
-        distance = 0
-
-        while queue:
-            size = len(queue)
-
-            # traverse by level
-            for i in range(size):
-                word = queue.popleft()
-                distances[word] = distance
-
-                next_words = self.get_next_words(word, dictionary)
-                mapping[word] = next_words
-
-                for next_word in next_words:
-                    if next_word not in visited:
-                        queue.append(next_word)
-                        visited.add(next_word)
-            distance += 1
-
-
-    def get_next_words(self, word, dictionary):
-
-        next_words = []
-        for i in range(len(word)):
-            for j in range(25):
-                index = (ord(word[i]) + j - ord('a')) % 26 + ord('a')
-                new_char = chr(index)
-                new_word = self.replace_char(i,new_char, word)
-
-                if new_word in dictionary:
-                    next_words.append(new_word)
-
-        return next_words
-
-    def replace_char(self, i, char, word):
-        return word[:i] + char + word[i + 1:]
 
 
 # def main():
