ee2587d7bd5b70ec9f66e137840b3ebf5c217f22
johnnyhsu1106 <johnnyhsu1106@gmail.com>
2019-01-27 21:56:53 -0500

    update solutions.

--- 109_triangle.py
+++ 109_triangle.py
@@ -2,12 +2,11 @@
 Given a triangle, find the minimum path sum from top to bottom.
 Each step you may move to adjacent numbers on the row below.
 
- Notice
+Notice
 
 Bonus point if you are able to do this using only O(n) extra space,
 where n is the total number of rows in the triangle.
 
-Have you met this question in a real interview? Yes
 Example
 Given the following triangle:
 
@@ -26,37 +25,41 @@ class Solution:
     @return: An integer, minimum path sum
     """
     def minimumTotal_1(self, triangle):
-        if not triangle or len(triangle) == 0:
+        if not triangle:
             return -1
 
-        # From Bottom to Top
         n = len(triangle)
+        # From Bottom to Top
         # initail the dp
         dp = [[0] * n  for i in range(n)]
-        # from n - 1 leel
-        for j in range(n):
-            dp[n-1][j] = triangle[n-1][j]
+        # in last row
+        for y in range(n):
+            dp[n - 1][y] = triangle[n - 1][y]
 
-        for i in range(n - 2, -1, -1): # from n - 2 level to 0 level
-            for j in range(i + 1): # from 0 to i
-                dp[i][j] = min(dp[i + 1][j] , dp[i + 1][j + 1]) + triangle[i][j]
+        for x in range(n - 2, -1, -1): # from n - 2 level to 0 level
+            for y in range(x + 1): # from 0 to x
+                dp[x][y] = min(dp[x + 1][y] , dp[x + 1][y + 1]) + triangle[x][y]
 
         return dp[0][0]
 
 
 
     def minimumTotal_2(self, triangle):
+        if not triangle:
+            return -1
+
         n = len(triangle)
+        # From Top to Bottom
         dp = [[0] * n  for i in range(n)]
-
         dp[0][0] = triangle[0][0]
-        for i in range(1, n):
-            dp[i][0] = dp[i - 1][0] + triangle[i][0]
-            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]
 
-        for i in range(1, n):
-            for j in range(1, i):
-                dp[i][j] = min(dp[i -1][j], dp[i - 1][j - 1]) + triangle[i][j]
+        for x in range(1, n):
+            dp[x][0] = dp[x - 1][0] + triangle[x][0]
+            dp[x][x] = dp[x - 1][x - 1] + triangle[x][x]
+
+        for x in range(1, n):
+            for y in range(1, x):
+                dp[x][y] = min(dp[x - 1][y], dp[x - 1][y - 1]) + triangle[x][y]
 
 
         return min(dp[n-1])
